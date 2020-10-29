76580a3ba061f286b71c7848cc4ef22784ae8aac
Aaron Varghese <39610813+7ROLI101@users.noreply.github.com>
2020-10-29 13:53:34 -0400

    Updated file to current

--- csv_input_creation.py
+++ csv_input_creation.py
@@ -6,6 +6,16 @@ surveyClass = sp.inputClass()
 surveyClass.set_input_class_info()
 surveyClass.set_class_info()
 with open('comments.csv',mode='w',newline='') as file:
-    csvwriter = csv.writer(file, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-    csvwriter.writerow(surveyClass.get_positive_comments())
-    csvwriter.writerow(surveyClass.get_negative_comments())
+    csvwriter = csv.writer(file, delimiter=',', quoting=csv.QUOTE_ALL)
+    positives = ['Valuable']
+    negatives = ['Needs Improvement']
+    csvwriter.writerow(['TYPE','COMMENT'])
+    for comment in surveyClass.get_positive_comments():
+        positives.append(comment)
+        csvwriter.writerow(positives)
+        del positives[1]
+
+    for comment in surveyClass.get_negative_comments():
+        negatives.append(comment)
+        csvwriter.writerow(negatives)
+        del negatives[1]
